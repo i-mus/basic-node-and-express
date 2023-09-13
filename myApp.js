801a378ea3ef614d344edc192177899be741563f
i-mus <mushthaqpullat@gmail.com>
2023-09-13 12:55:07 +0530

    changes

--- myApp.js
+++ myApp.js
@@ -6,15 +6,20 @@ let app = express();
 
 let absolutePath = __dirname + '/views/index.html'
 app.use("/public", express.static(__dirname + "/public"));
+
+
 app.get('/', function(req,res){
-    res.sendFile(absolutePath)
+    res.sendFile(absolutePath);
 })
+
+
 app.get('/json', function(req, res){
-    res.json({"message": "Hello json"})
+    res.json({"message": "Hello json"});
 })
-// app.get('/', function(req,res){
-//     res.send("Hello Express")
-// })
+
+
+
+
 
 
 
