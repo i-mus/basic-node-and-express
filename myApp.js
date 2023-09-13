caba359c1e42a1075efe9b28080d99ff23e758d9
i-mus <mushthaqpullat@gmail.com>
2023-09-13 12:32:25 +0530

    added style

--- myApp.js
+++ myApp.js
@@ -5,7 +5,7 @@ let app = express();
 // console.log("Hello World")
 
 let absolutePath = __dirname + '/views/index.html'
-
+app.use(express.static(__dirname + "/public"));
 app.get('/', function(req,res){
     res.sendFile(absolutePath)
 })
