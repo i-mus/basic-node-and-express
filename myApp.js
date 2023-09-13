a868ca2a1684172eee7e758f2d3ec09e9e67c262
i-mus <mushthaqpullat@gmail.com>
2023-09-13 12:41:28 +0530

    added json

--- myApp.js
+++ myApp.js
@@ -9,7 +9,9 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get('/', function(req,res){
     res.sendFile(absolutePath)
 })
-
+app.get('/json', function(req, res){
+    res.json({"message": "Hello json"})
+})
 // app.get('/', function(req,res){
 //     res.send("Hello Express")
 // })
